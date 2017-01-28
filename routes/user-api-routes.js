6768bdab5883ce6f9977b2f265b48174c3fbd387
Trung Ngo <trung@Trungs-MacBook-Pro.local>
2017-01-28 13:51:54 -0800

    configured auth

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -3,9 +3,10 @@ const passport = require('passport');
 module.exports = function(app) {
 
   app.post('/login',
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/profile');
-  });
+    passport.authenticate('local', { failureRedirect: '/something-else' }),
+    function(req, res) {
+
+      res.redirect('/profile');
+    });
 
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
-// var db = require('./db');
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -12,11 +12,14 @@ const PORT = process.env.PORT || 8080;
 // Requiring our models for syncing; commenting out until we have sequelize models
 var db = require("./models");
 
-passport.use(new Strategy(
-  function(email, password, cb) {
 
+passport.use(new Strategy({
+  usernameField: 'email'
+},
+  function(email, password, cb) {
     db.User.findOne({where: {email: email}})
     .then((user) => {
+      console.log(user.toJSON());
       // handle case where there is no existing user with the username
       if(!user) {
         return cb(null, false)
@@ -36,10 +39,14 @@ passport.use(new Strategy(
   }));
 
 passport.serializeUser(function(user, cb) {
+  // console.dir(user);
+  console.log(user.id);
+  console.log('serializeUser');
   cb(null, user.id);
 });
 
 passport.deserializeUser(function(id, cb) {
+  console.log('deserializeUser');
   db.User.findById(id)
   .then((user) => {
     cb(null, user);
@@ -50,13 +57,13 @@ passport.deserializeUser(function(id, cb) {
 });
 
 // Sets up the Express app to handle data parsing
+app.use(require('morgan')('combined'));
+// app.use(require('cookie-parser')()); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(require('morgan')('combined'));
-// app.use(require('cookie-parser')());
-// app.use(require('body-parser').urlencoded({ extended: true }));
+
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 // Set Handlebars as the default templating engine.
