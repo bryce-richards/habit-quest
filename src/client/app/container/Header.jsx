d888d5063dd5988d3227d04a141c2655fa9aa6bf
Trung Ngo <trung@Trungs-MacBook-Pro.local>
2017-02-01 22:35:01 -0800

    header, dashboardbody and dashboardview components rendering

---
+++ src/client/app/container/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { Button } from "react-bootstrap";
+// import ModalForm from "../container/ModalForm.jsx";
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>4 Week Challenge</h1>
+                <p>Live your life to the fullest, one week at a time</p>
+            </div>
+        );
+    }
+}
+
+export default Header;
