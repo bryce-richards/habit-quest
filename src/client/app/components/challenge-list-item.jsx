6aaf5b16b3afd3d88b00cf9f9ab3ebeba749fc77
Trung Ngo <trung@Trungs-MacBook-Pro.local>
2017-02-03 08:41:33 -0800

    put challenges in bootstrap grid, add button that links to /challenge/:challengeId

--- src/client/app/components/challenge-list-item.jsx
+++ src/client/app/components/challenge-list-item.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 const ChallengeListItem = ({challenge}) => {
+
+  const cardContentStyles = {
+    wordWrap: 'break-word',
+    border: '1px solid #333',
+    padding: '20px',
+    margin: '15px'
+  }
+  const challengeUrl = "challenge/" + challenge.id;
+
   return (
-    <div className="list-group-item" data-challengeId={challenge.id}>
-      <h3>{challenge.title}</h3>
-      <p>{challenge.description}</p>
+    <div className="challenge-card col-sm-6 col-md-4 col-xs-12 col-lg-4" data-challengeId={challenge.id}>
+      <div className="card-contents" style={cardContentStyles}>
+        <h3>{challenge.title}</h3>
+        <p>{challenge.description}</p>
+        <a href={challengeUrl} className="btn btn-primary">Challenge Details</a>
+      </div>
     </div>
   );
 }
