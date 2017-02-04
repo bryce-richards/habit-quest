05d332741b43d1c224aa7279e89cf94f97cf865a
Trung Ngo <trung@Trungs-MacBook-Pro.local>
2017-02-03 20:45:03 -0800

    tasks are being fetched when accessing /challenge/:id

--- src/client/app/components/ChallengeDetail.jsx
+++ src/client/app/components/ChallengeDetail.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
-const ChallengeDetail = (props) => {
-    return (
-      <div>
-        <h2>Challenge id: {props.params.id}</h2>
-      </div>
-    );
+class ChallengeDetail extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        tasks: []
+      };
+    }
+    componentWillMount() {
+      this.fetchTasks()
+    }
+
+    fetchTasks() {
+      var url = "/api/task/" + this.props.params.id;
+
+      var that = this;
+
+      axios.get(url).then((res) => {
+        that.setState({tasks: res.data.data});
+      }).catch((e) => {
+        return e
+      });
+
+    }
+
+    render() {
+      return (
+        <div>
+          <h2>Challenge id: {this.props.params.id}</h2>
+        </div>
+      );
+    }
 }
 
 export default ChallengeDetail;
--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -49883,24 +49883,73 @@
 	  value: true
 	});
 	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
 	var _react = __webpack_require__(/*! react */ 1);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	var ChallengeDetail = function ChallengeDetail(props) {
-	  return _react2.default.createElement(
-	    'div',
-	    null,
-	    _react2.default.createElement(
-	      'h2',
-	      null,
-	      'Challenge id: ',
-	      props.params.id
-	    )
-	  );
-	};
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var axios = __webpack_require__(/*! axios */ 236);
+	
+	var ChallengeDetail = function (_Component) {
+	  _inherits(ChallengeDetail, _Component);
+	
+	  function ChallengeDetail(props) {
+	    _classCallCheck(this, ChallengeDetail);
+	
+	    var _this = _possibleConstructorReturn(this, (ChallengeDetail.__proto__ || Object.getPrototypeOf(ChallengeDetail)).call(this, props));
+	
+	    _this.state = {
+	      tasks: []
+	    };
+	    return _this;
+	  }
+	
+	  _createClass(ChallengeDetail, [{
+	    key: 'componentWillMount',
+	    value: function componentWillMount() {
+	      this.fetchTasks();
+	    }
+	  }, {
+	    key: 'fetchTasks',
+	    value: function fetchTasks() {
+	      var url = "/api/task/" + this.props.params.id;
+	
+	      var that = this;
+	
+	      axios.get(url).then(function (res) {
+	        that.setState({ tasks: res.data.data });
+	      }).catch(function (e) {
+	        return e;
+	      });
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        _react2.default.createElement(
+	          'h2',
+	          null,
+	          'Challenge id: ',
+	          this.props.params.id
+	        ),
+	        console.log(this.state.tasks)
+	      );
+	    }
+	  }]);
+	
+	  return ChallengeDetail;
+	}(_react.Component);
 	
 	exports.default = ChallengeDetail;
 
