545c80fa4da8123865478360efc1a0f5fc059201
Bryce Richards <brichards5@gmail.com>
2017-02-04 00:24:06 -0800

    slowly building the individual task container

--- src/client/app/components/task-list-item.jsx
+++ src/client/app/components/task-list-item.jsx
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
+import { Panel, Grid, Col, Row, Button, ProgressBar } from "react-bootstrap";
+class TaskListItem extends Component {
+  constructor(props) {
+    super(props);
 
-const TaskListItem = ({task}) => {
+    this.state = {
+      task: this.props.task,
+      completedDays: this.props.task.completedDays,
+      targetComplete: this.props.task.targetComplete
+    }
+  }
 
-  return (
-  <li>{task.taskName}</li>
-  );
+  subtractDay() {
+    this.setState({targetComplete: this.state.targetComplete - 1});
+    // api route to update task
+  }
+
+  addDay() {
+    this.setState({targetComplete: this.state.targetComplete + 1});
+    // api route to update task
+  }
+  render() {
+    return (
+       <Panel header={this.props.task.taskName} 
+              bsStyle={this.state.completedDays < this.state.targetComplete ? "primary" : "success"}>
+          <Grid>
+              <Row>
+                  <Col lg={12}>
+                      <Row>
+                          <Col lg={4}>
+                            <p>Week: {this.props.task.week}</p>
+                          </Col>
+                          <Col lg={4}>
+                            <p>Target: {this.props.task.targetComplete}</p>
+                          </Col>
+                          <Col lg={4}>
+                            <p>Week {this.props.task.countComplete}</p>
+                          </Col>
+                      </Row>   
+                      <Row>
+                          <Col lg={2}>
+                              <Button onClick={this.subtractDay} 
+                                      bsStyle={this.state.completedDays > 0 ? "warning" : "danger"}
+                                      disabled={this.state.completedDays > 0 ? "false" : "true"}
+                              >
+                                  <i className="fa fa-minus-circle fa-3x" aria-hidden="true"></i>
+                              </Button>
+                          </Col>
+                              
+                          <Col lg={8}>
+                              <ProgressBar  active 
+                                  min={0} 
+                                  max={this.state.targetComplete} 
+                                  now={this.state.completedDays} 
+                                  label={this.state.completedDays}
+                              />
+                          </Col>
+                          <Col lg={2}>
+                              <Button onClick={this.addDay} 
+                                      bsStyle={this.state.completedDays < this.state.targetComplete ? "primary" : "success"}
+                                      disabled={this.state.completedDays < 7 ? "false" : "true"}
+                              >
+                                  <i className="fa fa-minus-circle fa-3x" aria-hidden="true"></i>
+                              </Button>
+                          </Col>
+                      </Row>
+                  </Col>
+              </Row>
+          </Grid>
+       </Panel> 
+      
+    )
+  }
 }
 
 export default TaskListItem;
