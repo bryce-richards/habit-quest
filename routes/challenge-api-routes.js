45ed7dbfc00151475b59e7f8e2d22a60ae7209fe
Trung Ngo <trung@Trungs-MacBook-Pro.local>
2017-02-03 23:56:48 -0800

    updated routes

--- routes/challenge-api-routes.js
+++ routes/challenge-api-routes.js
@@ -91,4 +91,66 @@ module.exports = function(app) {
         });
       });
 
+      // route to fetch all the active challenges
+    app.get("/api/challenge/active", (req, res) => {
+      var id = req.user.id;
+      db.Challenge.findAll({
+        where: {
+          activeChallenge: true,
+          UserId: id
+        }
+   }).then((data) => {
+         res.json({
+           success: true,
+           data: data
+         });
+       }).catch((e) => {
+         res.json({
+           success: false,
+           error: e
+         });
+       });
+     });
+
+   // route to fetch all the inactive challenges
+    app.get("/api/challenge/notActive", (req, res) => {
+      var id = req.user.id;
+      db.Challenge.findAll({
+        where: {
+          activeChallenge: false,
+          UserId: id
+        }
+   }).then((data) => {
+         res.json({
+           success: true,
+           data: data
+         });
+       }).catch((e) => {
+         res.json({
+           success: false,
+           error: e
+         });
+       });
+     });
+
+  // route to update if the challenge is currently active
+    app.put("/api/challenge/:challenge_id", (req,res) => {
+      db.Challenge.update({
+        activeChallenge: true
+      }, {
+        where: {
+          id: req.params.challenge_id
+        }
+      }).then((data) => {
+        res.json({
+          success: true,
+          data: data
+        });
+      }).catch((e) => {
+        res.json({
+          success: false,
+          error: e
+        });
+      });
+    });
 };
